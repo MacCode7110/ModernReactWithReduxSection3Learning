27f1c95a15790163b63bdfc24ecad270d0880716
Matthew McAlarney <memcalarney@wpi.edu>
2024-11-25 17:43:13 -0500

    Add images

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,9 +1,18 @@
-import ProfileCard from "./ProfileCard"
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistance</div>
+            <img src={AlexaImage} />
+            <img src={CortanaImage} />
+            <img src={SiriImage}/>
             <ProfileCard title = 'Alexa' handle = '@Alexa99'/>
             <ProfileCard title = 'Cortana' handle = '@Cortana32'/>
             <ProfileCard title = 'Siri' handle = '@Siri01'/>
